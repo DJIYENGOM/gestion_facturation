f59e34c7dde426e0728cbc64b958f747fe6e3c1e
Djiyé NGOM <ngomdjiye@gmail.com>
2024-05-14 12:44:19 +0000

    route pour lister les users

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('login_sousUtilisateur', 'login_sousUtilisateur');
     Route::post('logout_sousUtilisateur', 'logout_sousUtilisateur')->withoutMiddleware('auth:api');
     Route::post('refresh_sousUtilisateur', 'refresh_sousUtilisateur')->withoutMiddleware('auth:api'); //->withoutMiddleware('auth:api') permet de ne pas s'ecouter sur la table user;
+    Route::get('listerUser', 'listerUser');
 });
 
 
