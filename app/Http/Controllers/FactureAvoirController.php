52ade7e20eb099fc07863a769c181bcf72e161e9
Djiyé NGOM <ngomdjiye@gmail.com>
2024-07-08 11:56:12 +0000

    mis en jour de la documentation

---
+++ app/Http/Controllers/FactureAvoirController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FactureAvoir;
+use Illuminate\Http\Request;
+
+class FactureAvoirController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FactureAvoir $factureAvoir)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FactureAvoir $factureAvoir)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FactureAvoir $factureAvoir)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FactureAvoir $factureAvoir)
+    {
+        //
+    }
+}
---
+++ app/Models/FactureAvoir.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FactureAvoir extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'num_factureAvoir',
+        'facture_id',
+        'titreAccomp',
+        'dateAccompt',
+        'dateEcheance',
+        'montant',
+        'commentaire',
+        'devi_id',
+        'sousUtilisateur_id',
+        'user_id',
+    ];
+    public function facture()
+    {
+        return $this->belongsTo(Facture::class, 'facture_id');
+    }
+
+    public function devi()
+    {
+        return $this->belongsTo(Devi::class, 'devi_id');
+    }
+
+    public function sousUtilisateur()
+    {
+        return $this->belongsTo(Sous_Utilisateur::class, 'sousUtilisateur_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    
+}
