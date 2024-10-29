dfb9f169167a0b32c71d1687c7ab773dfa0c4a58
Djiyé NGOM <ngomdjiye@gmail.com>
2024-10-29 18:22:31 +0000

    gestion des cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,7 +19,7 @@ class CorsMiddleware
             $response = response('', 200);
             $response->headers->set('Access-Control-Allow-Origin', '*'); // Specify your frontend origin here
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization, Accept, charset, boundary, Confent-Length');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
             $response->headers->set('Access-Control-Allow-Credentials', 'true'); // Allow credentials
             return $response;
         }
@@ -27,7 +27,7 @@ class CorsMiddleware
         $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', '*'); // Specify your frontend origin here
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization, Accept, charset, boundary, Confent-Length');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true'); // Allow credentials
         return $response;
     }
