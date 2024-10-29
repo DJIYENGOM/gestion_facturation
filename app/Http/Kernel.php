9c1ed11ff8c772bdfb4521a69ac7814c26401d38
Djiyé NGOM <ngomdjiye@gmail.com>
2024-10-29 18:48:08 +0000

    gestion des cors(enlevement de toutes les cors)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\CorsMiddleware::class,
 
     ];
 
@@ -44,7 +43,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\CorsMiddleware::class,
+            // \App\Http\Middleware\CorsMiddleware::class,
 
         ],
     ];
@@ -68,10 +67,4 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
-
-    protected $routeMiddleware = [
-        // Other route middlewares...
-        'cors' => \App\Http\Middleware\CorsMiddleware::class,
-    ];
-    
 }
